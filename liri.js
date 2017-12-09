a325dd059a7a3fdbdfb2ebf6f33b3c864fc8479a
shulme801 <shulme801@gmail.com>
2017-12-09 07:58:12 -0500

    9 Dec Movies working

--- liri.js
+++ liri.js
@@ -33,15 +33,15 @@
 
 */ 
 
-
+/* The necessary requires */
 var keys 	  = require('./keys.js');
 var twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs 		  = require('fs');
-var myHTTP  = require('http');
 
 
+/*Twitter and Spotify objects to hold key info*/
 var tClient = new twitter({
 	consumer_key:        keys.consumer_key,
 	consumer_secret:     keys.consumer_secret,
@@ -54,10 +54,13 @@ var spotify = new Spotify({
   secret: '516c037d3f5448fab7918e5d402dc910'
 });
 
-var screenName = {screen_name: 'Real_SteveHulme'};
-var dividerLine = "\n++++++++++++++++++++++++\n";
+/* Global vars*/
+var screenName   = {screen_name: 'Real_SteveHulme'};
+var dividerLine  = "\n++++++++++++++++++++++++\n";
+var movieResults = [];
 
-/* grab the command line parameters from the process.argv array.  The first element is the node command and the second is the file name of this file
+/* The following grabs the command line parameters from the process.argv array and stores them into globals.  
+   The first element is the node command and the second is the file name of this file
    the actual Liri command we're interested in is at index 2 in the argv array. Following the Liri command will be the
    command's parameters (e.g., name of the song to spotify or name of the movie to put out the info for). We'll look for those parms
    when we process the Liri command.
@@ -71,12 +74,121 @@ var nodeParams = process.argv; // Copy those arguments off the command line.
 var liriCommand = cmdLineParams[liriCommandPos]; //Save off the command to Liri
 var liriParams = "";
 
+/* Utility Functions 
+*/
+function forgotParameter(liriCommand) {
+    console.log("Error -- command is "+liriCommand+" but no parameter supplied");
+    console.log("Try again!");
+}
 
-// Spotify info
-//Client ID af17d5b6dd4441abb9980e095aae3ac9
-//Client Secret 516c037d3f5448fab7918e5d402dc910
+function grabLiriParams() {
+
+  liriParams = cmdLineParams[3];
+
+  liriParams = cmdLineParams[liriParamPos]; //get that first parameter word from cmdLineParams
+  for (var i=liriParamPos+1; i<cmdLineParams.length; i++){ //start at second work in the parameter (if there is on)
+    liriParams += " "+cmdLineParams[i];
+  }
+
+}
+
+function logIt (logString){
+        //adds text to log.txt file
+        fs.appendFile('log.txt', logString,function(err) {
+          if (err) throw err;
+        });
+}
+
+function movieOutput (outStr) {
+  console.log(outStr);
+  outStr+="\n";
+  logIt(outStr);
+}
+
+/* Command Handlers */
+function showTweets(){
+  //Display last 20 Tweets and their dates
+
+  tClient.get('statuses/user_timeline', screenName, function(error, tweets, response){
+    if(!error){
+      for(var i = 0; i<tweets.length; i++){
+        var date = tweets[i].created_at;
+        console.log("@Real_SteveHulme: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log(dividerLine);
+        logString = "@Real_SteveHulme: " + tweets[i].text + " Created At: " + date.substring(0, 19)+dividerLine;
+        logIt(logString);
+      }
+    }else{
+      console.log('Error occurred');
+    }
+  });
+}
 
 
+
+function time2Spotify(song) {
+  
+  spotify.search({ type: 'track', query: song }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+    
+    for(var i = 0; i < data.tracks.items.length; i++){
+        var songData = data.tracks.items[i];
+        
+        console.log("Artist: " + songData.artists[0].name);
+        
+        console.log("Song: " + songData.name);
+        
+        console.log("Preview URL: " + songData.preview_url);
+        
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------")
+      }
+  });
+}
+  
+
+
+function getOMDBData(movie){
+
+ /* We're keeping it simple by using http.request to make the http call to the IMDB via omdbapi.com. 
+    A future improvement: use npm axios because it has some additional functionality.
+    Note that we use the omdbapi key that was obtained for this homework. */
+  var omdbURL = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=long&tomatoes=true&r=json&apikey=849535a0';
+  request(omdbURL, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+      // Store the results in an array of strings for now. These should become a movie object in the future.
+      
+      movieResults.push("Title: " + body.Title);
+      movieResults.push("Release Year: " + body.Year);
+      movieResults.push("IMdB Rating: " + body.imdbRating);
+      movieResults.push("Country: " + body.Country);
+      movieResults.push("Language: " + body.Language);
+      movieResults.push("Plot: " + body.Plot);
+      movieResults.push("Actors: " + body.Actors);
+      movieResults.push("Rotten Tomatoes Rating: " + body.tomatoRating);
+      movieResults.push("Rotten Tomatoes Rating: " + body.tomatoURL);
+
+      movieResults.forEach(movieOutput);
+      
+      console.log(dividerLine);
+      logIt(dividerLine);
+      
+    
+
+    } else {
+      console.log('Error occurred.')
+    }
+  })
+}
+
+function doCommand() {
+
+}
+
+/* Main line code*/
 switch(liriCommand) {
 	case "my-tweets":
 		 showTweets();
@@ -112,110 +224,8 @@ switch(liriCommand) {
 		break;
 }
 
-function forgotParameter(liriCommand) {
-		console.log("Error -- command is "+liriCommand+" but no parameter supplied");
-		console.log("Try again!");
-}
-
-function grabLiriParams() {
-
-	liriParams = cmdLineParams[3];
-
-	liriParams = cmdLineParams[liriParamPos]; //get that first parameter word from cmdLineParams
-	for (var i=liriParamPos+1; i<cmdLineParams.length; i++){ //start at second work in the parameter (if there is on)
-		liriParams += " "+cmdLineParams[i];
-	}
-
-}
-
-function logIt (logString){
-        //adds text to log.txt file
-        fs.appendFile('log.txt', logString,function(err) {
-          if (err) throw err;
-        });
-}
-
-function showTweets(){
-  //Display last 20 Tweets and their dates
-
-  tClient.get('statuses/user_timeline', screenName, function(error, tweets, response){
-    if(!error){
-      for(var i = 0; i<tweets.length; i++){
-        var date = tweets[i].created_at;
-        console.log("@Real_SteveHulme: " + tweets[i].text + " Created At: " + date.substring(0, 19));
-        console.log(dividerLine);
-        logString = "@Real_SteveHulme: " + tweets[i].text + " Created At: " + date.substring(0, 19)+dividerLine;
-        logIt(logString);
-      }
-    }else{
-      console.log('Error occurred');
-    }
-  });
-}
 
 
 
-function time2Spotify(song) {
-	
-  spotify.search({ type: 'track', query: song }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-    
-    for(var i = 0; i < data.tracks.items.length; i++){
-        var songData = data.tracks.items[i];
-        
-        console.log("Artist: " + songData.artists[0].name);
-        
-        console.log("Song: " + songData.name);
-        
-        console.log("Preview URL: " + songData.preview_url);
-        
-        console.log("Album: " + songData.album.name);
-        console.log("-----------------------")
-      }
-  });
-}
-  
-
-
-function getOMDBData(movie){
-
-console.log('Movie Title is '+movie);
-var omdbURL = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=long&tomatoes=true&r=json&apikey=849535a0';
-  myHTTP.request(omdbURL, function (error, response, body){
-    if(!error && response.statusCode == 200){
-      var body = JSON.parse(body);
-
-      console.log("Title: " + body.Title);
-      console.log("Release Year: " + body.Year);
-      console.log("IMdB Rating: " + body.imdbRating);
-      console.log("Country: " + body.Country);
-      console.log("Language: " + body.Language);
-      console.log("Plot: " + body.Plot);
-      console.log("Actors: " + body.Actors);
-      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-
-      //adds text to log.txt
-      fs.appendFile('log.txt', "Title: " + body.Title);
-      fs.appendFile('log.txt', "Release Year: " + body.Year);
-      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-      fs.appendFile('log.txt', "Country: " + body.Country);
-      fs.appendFile('log.txt', "Language: " + body.Language);
-      fs.appendFile('log.txt', "Plot: " + body.Plot);
-      fs.appendFile('log.txt', "Actors: " + body.Actors);
-      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
-
-    } else {
-      console.log('Error occurred.')
-    }
-  })
-}
-
-function doCommand() {
-
-}
 
 
